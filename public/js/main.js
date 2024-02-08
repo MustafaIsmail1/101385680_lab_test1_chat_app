169f82c82870f1165c84f2a2881f053b87593f6e
Mustafa Ismail <mustafaismailmab@gmail.com>
2024-02-07 22:10:15 -0500

    second commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,31 @@
+const socket = io();
+
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
-// Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
-const socket = io();
-
-// Join chatroom
 socket.emit('joinRoom', { username, room });
 
-// Get room and users
 socket.on('roomUsers', ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
+  displayRoomName(room);
+  displayUsers(users);
 });
 
-// Message from server
 socket.on('message', (message) => {
   console.log(message);
-  outputMessage(message);
+  displayMessage(message);
 
-  // Scroll down
   chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
-// Message submit
 chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  // Get message text
   let msg = e.target.elements.msg.value;
 
   msg = msg.trim();
@@ -41,22 +34,18 @@ chatForm.addEventListener('submit', (e) => {
     return false;
   }
 
-  // Emit message to server
   socket.emit('chatMessage', msg);
 
-  // Clear input
   e.target.elements.msg.value = '';
   e.target.elements.msg.focus();
 });
 
-// Output message to DOM
-function outputMessage(message) {
+function displayMessage(message) {
   const div = document.createElement('div');
   div.classList.add('message');
   const p = document.createElement('p');
   p.classList.add('meta');
   p.innerText = message.username;
-  //p.innerHTML += `<span>${message.time}</span>`;
   div.appendChild(p);
   const para = document.createElement('p');
   para.classList.add('text');
@@ -65,13 +54,11 @@ function outputMessage(message) {
   document.querySelector('.chat-messages').appendChild(div);
 }
 
-// Add room name to DOM
-function outputRoomName(room) {
+function displayRoomName(room) {
   roomName.innerText = room;
 }
 
-// Add users to DOM
-function outputUsers(users) {
+function displayUsers(users) {
   userList.innerHTML = '';
   users.forEach((user) => {
     const li = document.createElement('li');
@@ -80,11 +67,7 @@ function outputUsers(users) {
   });
 }
 
-//Prompt the user before leave chat room
 document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
-  if (leaveRoom) {
     window.location = '../index.html';
-  } else {
   }
-});
+);
